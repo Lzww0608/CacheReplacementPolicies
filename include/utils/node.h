1194d8e2bfb384d9b63610a38346979a3c4214f3
Lzww <63537700+Lzww0608@users.noreply.github.com>
2025-07-24 16:36:10 +0800

    Update node.h

--- include/utils/node.h
+++ include/utils/node.h
@@ -10,21 +10,30 @@
 
 #include <chrono>
 
+// 侵入式链表节点基类（提供 prev/next 指针）
+template <typename T>
+struct IntrusiveListNode {
+    T* prev;  // 前向指针
+    T* next;  // 后向指针
+
+    // 构造函数：初始化指针为 nullptr
+    IntrusiveListNode() : prev(nullptr), next(nullptr) {}
+};
+
 template <typename K, typename V>
-struct Node {
+struct Node: public IntrusiveListNode<Node<K, V>> {
     K key;
     V value;
-    Node* prev;
-    Node* next;
     std::chrono::steady_clock::time_point expire_time;
     uint64_t frequency = 1; // LFU访问频率
     uint8_t clock_bit = 0; // Clock算法位
+    bool is_in_protected;  // 标记是否在 protected 区
 
     // 构造函数
     Node() : prev(nullptr), next(nullptr) {}
     
     Node(const K& k, const V& v, int expire_ms = 3600000) 
-        : key(k), value(v), prev(nullptr), next(nullptr) {
+        : key(k), value(v), prev(nullptr), next(nullptr), is_in_protected(false) {
         if (expire_ms > 0) {
             expire_time = std::chrono::steady_clock::now() + std::chrono::milliseconds(expire_ms);
         } else {
