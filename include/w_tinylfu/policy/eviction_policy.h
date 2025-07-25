592490189b60097946b6f5c2b91c00acb625041a
Lzww <63537700+Lzww0608@users.noreply.github.com>
2025-07-25 15:01:40 +0800

    Update eviction_policy.h

--- include/w_tinylfu/policy/eviction_policy.h
+++ include/w_tinylfu/policy/eviction_policy.h
@@ -5,43 +5,55 @@
 #include "../../utils/node.h"
 
 #include <cstdint>
+#include <unordered_map>
+#include <hash>
+#incldue <string>
 
 namespace CRP {
 namespace w_tinylfu {
 
-
-template <typename K, typename V>
+template <typename K, typename V, typename Hash = std::hash<std::string>>
 class SLRU {
 public:
     using Node = ::Node<K, V>;
+    using List = ::IntrusiveList<K, V>;
 
     virtual ~SLRU() = default;
     /* 从probation 升至 protected */
-    void onAccess(Node* n);
+    void OnAccess(Node* n);
     /* 从窗口缓存升至 probation */
-    void onAdd(Node* n);
+    void OnAdd(Node* n);
 
     /* 移除节点 */
-    uint32_t erase(Node* node);
+    uint32_t EraseNode(Node* node);
 
     /* 获取victime节点 */
-    Node* getVictim();
+    Node* GetVictim();
 
     /* 公平竞争：候选数据 与 受害者*/
-    uint32_t compete(Node* candidate, Node* victim);
+    uint32_t Compete(Node* candidate, Node* victim);
     /* 驱逐节点 */
-    Node* evict();
+    Node* Evict();
 
     /* 获取probation和protection大小 */
-    uint64_t getProbationSize() const;
-    uint64_t getProtectionSize() const;
+    uint64_t GetProbationSize() const;
+    uint64_t GetProtectionSize() const;
+    uint64_t GetSize() const;
+    uint64_t GetCapacity() const;
 
+    /* 判断节点是否存在于主缓存 */
+    bool Contains(const K& key) const;
 private:
-    IntrusiveList<K, V> probation_;
-    IntrusiveList<K, V> protection_;
+    List probation_;
+    List protection_;
     
     uint64_t probation_size_;
     uint64_t protection_size_;
+
+    uint64_t probation_capacity_;
+    uint64_t protection_capacity_;
+
+    std::unordered_map<K, Node*> key_to_node_;
 };
 
 } // namespace w_tinylfu
