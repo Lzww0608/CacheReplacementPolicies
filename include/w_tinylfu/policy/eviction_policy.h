8109b04751e2babeb1ebae277769467a4516bfbf
Lzww <63537700+Lzww0608@users.noreply.github.com>
2025-08-04 11:15:18 +0800

    Update eviction_policy.h

--- include/w_tinylfu/policy/eviction_policy.h
+++ include/w_tinylfu/policy/eviction_policy.h
@@ -24,30 +24,30 @@ public:
     SLRU(uint64_t probation_capacity, uint64_t protection_capacity);
     virtual ~SLRU() = default;
     /* 从probation 升至 protected */
-    void OnAccess(Node* n);
+    void onAccess(Node* n);
     /* 从窗口缓存升至 probation */
-    void OnAdd(Node* n);
+    void onAdd(Node* n);
 
     /* 移除节点 */
-    uint32_t EraseNode(Node* node);
+    uint32_t eraseNode(Node* node);
 
     /* 公平竞争：候选数据 与 受害者*/
-    uint32_t Compete(Node* candidate, Node* victim);
+    uint32_t compete(Node* candidate, Node* victim);
     /* 驱逐节点 */
-    Node* Evict();
+    Node* evict();
 
     bool get(const K& key, V& value);
 
     void put(const K& key, const V& value);
 
     /* 获取probation和protection大小 */
-    uint64_t GetProbationSize() const;
-    uint64_t GetProtectionSize() const;
-    uint64_t GetSize() const;
-    uint64_t GetCapacity() const;
+    uint64_t probationSize() const;
+    uint64_t protectionSize() const;
+    uint64_t size() const;
+    uint64_t capacity() const;
 
     /* 判断节点是否存在于主缓存 */
-    bool Contains(const K& key) const;
+    bool contains(const K& key) const;
 
     /* protected_ decay*/
     void decay_all_frequencies(double factor);
