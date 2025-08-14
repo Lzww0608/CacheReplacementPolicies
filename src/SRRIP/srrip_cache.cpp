8fb490b29fd9ba7b5ec0971390e0429e37994385
Lzww <63537700+Lzww0608@users.noreply.github.com>
2025-08-14 19:34:12 +0800

    Update srrip_cache.cpp

--- src/SRRIP/srrip_cache.cpp
+++ src/SRRIP/srrip_cache.cpp
@@ -14,7 +14,7 @@
 namespace SRRIP {
 
 template <uint8_t RRPV_M_BITS>
-SRRIPCache::SRRIPCache(size_t cache_size_kb, size_t block_size_bytes, size_t associativity)
+SRRIPCache<RRPV_M_BITS>::SRRIPCache(size_t cache_size_kb, size_t block_size_bytes, size_t associativity)
     : associativity_(associativity) {
     if (cache_size_kb == 0 || block_size_bytes == 0 || associativity == 0) {
         throw std::invalid_argument("Cache paramenters must b positive");
@@ -51,5 +51,44 @@ SRRIPCache::SRRIPCache(size_t cache_size_kb, size_t block_size_bytes, size_t ass
     sets_.resize(num_sets_, CacheSet<RRPV_M_BITS>(associativity));
 }
 
+template <uint8_t RRPV_M_BITS>
+void SRRIPCache<RRPV_M_BITS>::parseAddress(uint64_t address, uint64_t& tag, size_t& set_index) const {
+    set_index = (address >> offset_bits_) & ((1ULL << set_index_bits_) - 1);
+    tag = address >> (offset_bits_ + set_index_bits_);
+}
+
+template <uint8_t RRPV_M_BITS>
+bool SRRIPCache<RRPV_M_BITS>::access(uint64_t address) {
+    uint64_t tag;
+    size_t set_index;
+    parseAddress(address, tag, set_index);
+
+    assert(set_index < num_sets_ && "Invalid set index");
+
+    auto& target_set = sets_[set_index];
+    auto way = target_set.findWay(tag);
+
+    if (way.has_value()) {
+        target_set.accessWay(way.value());
+        hits_count_.fetch_add(1, std::memory_order_relaxed);
+        return true;
+    } else {
+        miss_count_.fetch_add(1, std::memory_order_relaxed);
+
+        // 1. find empty block
+        auto empty_way = target_set.findEmptyWay();
+        if (empty_way.has_value()) {
+            target_set.fillWay(empty_way.value(), tag);
+            return false;
+        }
+
+        // 2. find victim and replace it 
+        size_t victim_way = target_set.findVictimWay();
+        target_set.fillWay(victim_way, tag);
+        return false;
+    }
+}
+
 
 } // namespace SRRIP
+
